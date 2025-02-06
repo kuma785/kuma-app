fb8d1667a5e1e1dfc46b3a886626a3f608a2f2a2
mayu kuwamura <kuwamura@atma.co.jp>
2025-02-07 08:14:53 +0900

    画像削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { useState, useEffect } from "react";
 import liff from "@line/liff"; // LIFF SDKをインポート
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 const LIFF_ID = "2006857062-gVZvRdqj"; // LINE Developersで発行したLIFF IDを設定
@@ -31,14 +29,6 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + Reactアプリ</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
