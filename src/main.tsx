8b9470e8270be56358fa8e8de2d3e5580fa939e7
mayu kuwamura <kuwamura@atma.co.jp>
2025-02-07 09:44:36 +0900

    スクリプト修正

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,11 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+console.log("Root element exists:", document.getElementById('root'));
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <p>sssss</p>
   </StrictMode>,
 )
